671e630e1dee8dd8148d43359a4895d941de1356
Rosario Delgado <rdr12@hotmail.es>
2022-04-19 00:56:50 +0200

    LabVikingos

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,56 @@
 // Soldier
-class Soldier {}
+class Soldier {
+
+  constructor(healthParaman, strengthParaman) {
+    this.health = healthParaman;
+    this.strength = strengthParaman;
+  }
+
+  attack = () => {
+    return this.strength;
+  }
+  receiveDamage = (damageParameter) => {
+  this.health -= damageParameter;
+ };
+}
 
 // Viking
-class Viking {}
+
+class Viking extends Soldier {
+constructor(nameParaman, healthParaman, strengthParaman) {
+  super(healthParaman, strengthParaman);
+  this.name = nameParaman;
+ }
+
+receiveDamage = (damageParameter) => {
+this.health = this.health - damageParameter;
+ if (this.health > 0) {
+ return `${this.name} has received ${damageParameter} points of damage;`
+} else {
+  return `${this.name} has died in act of combat`;
+
+ }
+};
+
+battleCry = () => {
+ return "Odin Owns You All!";
+
+ };
+}
+  
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage=(damageParameter) => {
+    this.health = this.health - damageParameter
+    if (this.health > 0){
+      return `A Saxon has receive ${damageParameter} points of damage` 
+    } else {
+      return "A Saxon has died in combat"
+    }
+  }
+}
 
 // War
 class War {}
